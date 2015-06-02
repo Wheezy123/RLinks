1c8eff7b54981ce76309fe2cdee74edf52de7059
Wheezy123 <johnsonjeromec@gmail.com>
2015-06-01 22:04:47 -0500

    add and configured links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,6 +4,14 @@ class LinksController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @links = @user.links.new(link_params)
+
+    if @link.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -11,6 +19,13 @@ class LinksController < ApplicationController
 
   def destroy
   end
+
+
+    private
+
+      def link_params
+        params.require(:link).permit(:title, :url)
+      end
 
 
 end
